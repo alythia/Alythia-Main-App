fa62c8f8a0bb4e1ae4c745405d547b87dfc8f4c9
Matt Ferguson <mdfergus@gmail.com>
2018-06-20 16:32:57 -0400

    package-lock changes

--- client/components/client-landing.js
+++ client/components/client-landing.js
@@ -2,13 +2,17 @@ import React, {Component} from 'React'
 import Project from './project-card'
 
 export default class Landing extends Component {
+  state = {
+    userInfo: []
+  }
+
   render() {
     return (
       <div>
         <div>Welcome to app</div>
         <div>This is info here:</div>
         <div>
-          <Project />
+          <Project userInfo={this.state.userInfo} />
         </div>
       </div>
     )
