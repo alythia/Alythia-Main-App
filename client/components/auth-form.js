988a8d6d884cfc9ec02997d315f7e878f6f77e3e
Dave Trabka <dtrabka@gmail.com>
2018-06-21 12:12:02 -0400

    feat(auth-form): tweaked auth login

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
+import {Input} from 'react-materialize'
 import {auth} from '../store'
 
-/**
- * COMPONENT
- */
 const AuthForm = props => {
   const {name, displayName, handleSubmit, error} = props
 
   return (
-    <div>
+    <div className="container">
       <form onSubmit={handleSubmit} name={name}>
         <div>
-          <label htmlFor="email">
-            <small>Email</small>
-          </label>
-          <input name="email" type="text" />
+          <Input
+            type="email"
+            name="email"
+            htmlFor="email"
+            label="Email"
+            s={12}
+          />
         </div>
         <div>
           <label htmlFor="password">
@@ -28,19 +29,17 @@ const AuthForm = props => {
           <button type="submit">{displayName}</button>
         </div>
         {error && error.response && <div> {error.response.data} </div>}
+        <a href="/auth/google">
+          <div className="btn_google center">
+            <img src="/img/btn_google.svg" />
+            <p>{displayName} with Google</p>
+          </div>
+        </a>
       </form>
-      <a href="/auth/google">{displayName} with Google</a>
     </div>
   )
 }
 
-/**
- * CONTAINER
- *   Note that we have two different sets of 'mapStateToProps' functions -
- *   one for Login, and one for Signup. However, they share the same 'mapDispatchToProps'
- *   function, and share the same Component. This is a good example of how we
- *   can stay DRY with interfaces that are very similar to each other!
- */
 const mapLogin = state => {
   return {
     name: 'login',
@@ -72,9 +71,6 @@ const mapDispatch = dispatch => {
 export const Login = connect(mapLogin, mapDispatch)(AuthForm)
 export const Signup = connect(mapSignup, mapDispatch)(AuthForm)
 
-/**
- * PROP TYPES
- */
 AuthForm.propTypes = {
   name: PropTypes.string.isRequired,
   displayName: PropTypes.string.isRequired,
