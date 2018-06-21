6379727baca231eea1242ef314a3ba675434c0e5
Matt Ferguson <mdfergus@gmail.com>
2018-06-21 11:32:48 -0400

    feature(client add new project) adding forms and format to new project component

--- client/routes.js
+++ client/routes.js
@@ -19,7 +19,7 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route path="/" component={Landing} />
+        <Route path="/" exact component={Landing} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         {isLoggedIn && (
