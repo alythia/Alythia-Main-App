392cbe01ec6410d9249acd522e7d646f350f17d3
Ziyovuddin <z.shamsiddini@gmail.com>
2018-06-21 18:00:28 -0400

    feat(qr-generator): add peoper token to generate and proper UX

--- client/components/qrcode-landing-page.js
+++ client/components/qrcode-landing-page.js
@@ -1,39 +1,76 @@
 /* eslint-disable react/prefer-stateless-function */
 'use strict'
 import React, {Component} from 'react'
-import sha256 from 'sha256'
 import UniqueQRCode from './qrcode-generator'
-import generateUUID from '../uuid-generator'
+import axios from 'axios'
+// import generateUUID from '../uuid-generator'
 
 class QRCodeLanding extends Component {
-  generateRandomHash = () => {
-    const UUID = generateUUID()
-    return sha256(UUID)
+  constructor() {
+    super()
+    this.state = {
+      pageLoaded: false,
+      token: '',
+      message: ''
+    }
+  }
+
+  async componentDidMount() {
+    const token = this.props.location.search.split('=')[1]
+    try {
+      const result = await axios.post('/api/clients/verify', {token})
+      if (result.status === 200) {
+        this.setState({
+          token,
+          pageLoaded: true
+        })
+      }
+    } catch (error) {
+      console.log(error)
+      this.setState({
+        pageLoaded: true,
+        message: error.message
+      })
+    }
   }
 
   render() {
-    const apiToken = this.props.location.search.split('=')[1]
-    const randomHash = this.generateRandomHash()
     // TODO: discuss how to give this a 10 minute shelf life
+    const {token, message, pageLoaded} = this.state
 
-    console.log(apiToken)
-    return (
-      <div className="row container center">
-        <br />
-        <h5 className="header">Scan to Authenticate with Alythia...</h5>
-        <br />
-        <UniqueQRCode
-          id="QRCodeRender"
-          apiToken={apiToken}
-          randomHash={randomHash}
-        />
-        <br />
-        <br />
-        <h5 className="grey-text text-darken-3 lighten-3">
-          ...and continue to [www.placeholder.com]
-        </h5>
-      </div>
-    )
+    if (!pageLoaded) {
+      return (
+        <div className="row container center">
+          <br />
+          <div className="progress">
+            <div className="indeterminate" />
+          </div>
+          <br />
+        </div>
+      )
+    } else if (pageLoaded && token.length > 0) {
+      return (
+        <div className="row container center">
+          <br />
+          <h5 className="header">Scan to Authenticate with Alythia...</h5>
+          <br />
+          {<UniqueQRCode id="QRCodeRender" apiToken={token} />}
+          <br />
+          <br />
+          <h5 className="grey-text text-darken-3 lighten-3">
+            ...and continue to [www.placeholder.com]
+          </h5>
+        </div>
+      )
+    } else if (pageLoaded && message.length > 0) {
+      return (
+        <div className="row container center">
+          <br />
+          <h5 className="header">{message}</h5>
+          <br />
+        </div>
+      )
+    }
   }
 }
 
