f19911acce5f8248c1a5f6cf77eb17ee68e10c9c
Dave Trabka <dtrabka@gmail.com>
2018-06-20 21:44:08 -0400

    feat(qrcode-render): built rendering QR code page based on passed in indentifying value (ie api token via url) and generates a random hashed identifier for the transaction before rendering that combined data into the qr code

--- client/routes.js
+++ client/routes.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, DeveloperHome} from './components'
+import QRCodeLanding from './components/qrcode-landing-page'
 import {me} from './store'
 
 /**
@@ -21,6 +22,7 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route path="/auth/identify" component={QRCodeLanding} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
