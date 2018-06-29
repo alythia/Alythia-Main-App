1b3f9bc671c43a17c0025217db5fe68a8ff2aae1
Dave Trabka <dtrabka@gmail.com>
2018-06-29 10:42:46 -0400

    feat(verification and redis): routes and verifiers added, redis implemented

--- server/api/index.js
+++ server/api/index.js
@@ -3,15 +3,15 @@ module.exports = router
 
 router.use('/developers', require('./developers'))
 router.use('/users', require('./users'))
-router.use('/clients', require('./clients'))
+router.use('/clients', require('./clients').router)
 
 router.get('/test-qr', (req, res, next) => {
-  res.send('Testing successful for GET request')  
+  res.send('Testing successful for GET request')
 })
 
 router.post('/test-qr', (req, res, next) => {
-  console.log('BODY CONTAINS', req.body);
-  res.send('Testing successful for POST request')  
+  console.log('BODY CONTAINS', req.body)
+  res.send('Testing successful for POST request')
 })
 
 router.use((req, res, next) => {
