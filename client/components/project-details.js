22cf61df604b7f9cd348e997adbb04663d310357
Ziyovuddin <z.shamsiddini@gmail.com>
2018-06-29 01:24:18 -0400

    fix(modal): fixed modal not updating project info

--- client/components/project-details.js
+++ client/components/project-details.js
@@ -1,47 +1,68 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Input} from 'react-materialize'
+import {connect} from 'react-redux'
 
-export const ProjectDetails = props => {
-  return (
-    <form className="col m4 s6">
-      <Input
-        s={6}
-        label="Your Client ID"
-        name="projectID"
-        defaultValue={props.userInfo.client_id}
-        disabled
-      />
-      <Input
-        s={6}
-        label="Project Name"
-        name="projectName"
-        defaultValue={props.userInfo.projectName}
-        disabled
-      />
-      <Input
-        s={6}
-        label="Project URL"
-        name="projectURL"
-        defaultValue={props.userInfo.website}
-        disabled
-      />
-      <Input
-        s={6}
-        label="Public Key"
-        name="apiToken"
-        type="textarea"
-        id="apiTextArea"
-        defaultValue={props.userInfo.public_key}
-        disabled
-      />
-      <Input
-        s={6}
-        label="Secret Key"
-        name="clientSecret"
-        defaultValue={props.userInfo.secret_key}
-        disabled
-      />
-    </form>
-  )
+class ProjectDetails extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    let {currentProject} = this.props
+    console.log('CURRENT PROJECT', currentProject)
+    return (
+      <form className="col m4 s6">
+        <Input
+          s={6}
+          label="Your Client ID"
+          name="projectID"
+          placeholder=""
+          value={currentProject.client_id}
+          disabled
+        />
+        <Input
+          s={6}
+          label="Project Name"
+          name="projectName"
+          placeholder=""
+          value={currentProject.projectName}
+          disabled
+        />
+        <Input
+          s={6}
+          label="Project URL"
+          name="projectURL"
+          placeholder=""
+          value={currentProject.website}
+          disabled
+        />
+        <Input
+          s={6}
+          label="Public Key"
+          name="apiToken"
+          type="textarea"
+          id="apiTextArea"
+          placeholder=""
+          value={currentProject.public_key}
+          disabled
+        />
+        <Input
+          s={6}
+          label="Secret Key"
+          name="clientSecret"
+          placeholder=""
+          value={currentProject.secret_key}
+          disabled
+        />
+      </form>
+    )
+  }
 }
-export default ProjectDetails
+
+const mapStateToProps = state => {
+  return {
+    currentProject: state.client.currentProject
+  }
+}
+
+export default connect(mapStateToProps)(ProjectDetails)
