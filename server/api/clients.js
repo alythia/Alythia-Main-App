e138ccc5d956622ecbe0a8c5d2d7367f91080ef7
Dave Trabka <dtrabka@gmail.com>
2018-06-29 23:21:47 -0400

    feat(landing): adjusted landing page to include Alythia description instead of adding project

--- server/api/clients.js
+++ server/api/clients.js
@@ -15,20 +15,20 @@ const jwt = require('jsonwebtoken')
 //     'redis://h:p0adc8345fd36407381319fa474d9bb82a952ca7fbc237d770b321799c7fd6365@ec2-52-23-66-23.compute-1.amazonaws.com:35149'
 // })
 
-// const redisClient = redis.createClient({
-//   host: 'localhost',
-//   port: 6379
-// })
+const redisClient = redis.createClient({
+  host: 'localhost',
+  port: 6379
+})
 
-// redisClient.on('ready', function() {
-//   console.log('Redis is ready')
-// })
+redisClient.on('ready', function() {
+  console.log('Redis is ready')
+})
 
-// redisClient.on('error', function(err) {
-//   console.log(
-//     'error event - ' + redisClient.host + ':' + redisClient.port + ' - ' + err
-//   )
-// })
+redisClient.on('error', function(err) {
+  console.log(
+    'error event - ' + redisClient.host + ':' + redisClient.port + ' - ' + err
+  )
+})
 
 // -------------------------------------------------------------
 
@@ -84,11 +84,11 @@ router.get('/:client_id', async (req, res, next) => {
       where: {client_id}
     })
 
-//     await redisClient.set(UUID, client_id, function(err, reply) {
-//       err
-//         ? console.log('Redis error on SET: ', err)
-//         : console.log('Redis SET: ', `${UUID}: ${client_id}`)
-//     })
+    await redisClient.set(UUID, client_id, function(err, reply) {
+      err
+        ? console.log('Redis error on SET: ', err)
+        : console.log('Redis SET: ', `${UUID}: ${client_id}`)
+    })
 
     const {secret_key, public_key, projectName, website} = client
     const result = {
