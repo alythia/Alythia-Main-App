698208ca0db5d703a57cc981b92864b0b21781d0
Dave Trabka <dtrabka@gmail.com>
2018-07-03 10:23:56 -0400

    feat(qr-code): updated payload to include project name and URL

--- server/api/clients.spec.js
+++ server/api/clients.spec.js
@@ -1,43 +1,43 @@
-/* global describe beforeEach it */
+// /* global describe beforeEach it */
 
-const {expect} = require('chai')
-const request = require('supertest')
-const db = require('../db')
-const app = require('../index')
-const Client = db.model('client')
+// const {expect} = require('chai')
+// const request = require('supertest')
+// const db = require('../db')
+// const app = require('../index')
+// const Client = db.model('client')
 
-describe('Client routes', () => {
-  beforeEach(() => {
-    return db.sync({force: true})
-  })
+// describe('Client routes', () => {
+//   beforeEach(() => {
+//     return db.sync({force: true})
+//   })
 
-  describe('/api/clients/register/', () => {
-    // const codysProject = 'puppybook'
-    // const codysWebsite = 'puppybook.com'
+//   describe('/api/clients/new-project/', () => {
+//     // const codysProject = 'puppybook'
+//     // const codysWebsite = 'puppybook.com'
 
-    beforeEach(() => {
-      return Client.create({
-        projectName: 'puppybook',
-        website: 'www.puppybook.com'
-      })
-    })
+//     beforeEach(() => {
+//       return Client.create({
+//         projectName: 'puppybook',
+//         website: 'www.puppybook.com'
+//       })
+//     })
 
-    it('GET /api/clients/register', async () => {
-      const res = await request(app)
-        .get('/api/clients/')
-        .expect(200)
+//     it('GET /api/clients/new-project', async () => {
+//       const res = await request(app)
+//         .get('/api/clients/')
+//         .expect(200)
 
-      expect(res.body).to.be.an('array')
-      expect(res.body[0].projectName).to.be.equal('puppybook')
-    })
+//       expect(res.body).to.be.an('array')
+//       expect(res.body[0].projectName).to.be.equal('puppybook')
+//     })
 
-    xit('GET /api/clients/register', async () => {
-      const res = await request(app)
-        .get('/api/clients/')
-        .expect(200)
+//     xit('GET /api/clients/register', async () => {
+//       const res = await request(app)
+//         .get('/api/clients/')
+//         .expect(200)
 
-      expect(res.body).to.be.an('array')
-      expect(res.body[0].website).to.be.equal('www.puppybook.com')
-    })
-  }) // end describe('/api/clients')
-}) // end describe('Client routes')
+//       expect(res.body).to.be.an('array')
+//       expect(res.body[0].website).to.be.equal('www.puppybook.com')
+//     })
+//   }) // end describe('/api/clients')
+// }) // end describe('Client routes')
--- server/api/users.js
+++ server/api/users.js
@@ -5,20 +5,6 @@ const {redisClient} = require('../redis')
 
 module.exports = router
 
-router.get('/', async (req, res, next) => {
-  try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['UUID', 'email']
-    })
-    res.json(users)
-  } catch (err) {
-    next(err)
-  }
-})
-
 router.post('/', async (req, res, next) => {
   try {
     const result = await User.create(req.body)
@@ -32,6 +18,7 @@ router.post('/', async (req, res, next) => {
 router.post('/verify/', async (req, res, next) => {
   try {
     res.sendStatus(200)
+
     // Verify user
     const userEmail = req.body.email
     const userIdentifier = req.body.userIdentifier
@@ -39,7 +26,6 @@ router.post('/verify/', async (req, res, next) => {
     const transactionIdentifier = req.body.transactionIdentifier
 
     const user = await User.findOne({where: {email: userEmail}})
-
     if (!user) {
       console.log('User not found:', userEmail)
       res.status(401).send(`Access denied, user does not exist: ${userEmail}`)
@@ -49,18 +35,11 @@ router.post('/verify/', async (req, res, next) => {
     }
 
     // Verify client and post to Client backend
-
-    console.log('TRANSACTION ID: ', transactionIdentifier)
-    console.log('CLIENT ID: ', clientIdentifier)
-
     await redisClient.get(transactionIdentifier, async function(err, reply) {
       if (err) {
         console.log('Redis error on GET: ', err)
       } else {
         console.log('Redis reply on GET: ', reply)
-        console.log('HERES MY USER: ', user)
-        console.log('CLIENT ID LOOK UP SUCCESS?: ', clientIdentifier === reply)
-
         if (user && clientIdentifier === reply) {
           // After user and client are verified, post to client user email
           const {data} = await axios.post(
